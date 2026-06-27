e80f8b61fe93fde001a11a040f3b99f1c80a22a2
user.name <vioanakinyi06@gmail.com>
2026-06-27 19:12:02 +0300

    Update env variables

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors({origin:"http://localhost:3000"}));
+app.use(cors({origin:"https://jomange-pos.vercel.app/"}));
 app.use(express.json());
 
 // Routes
